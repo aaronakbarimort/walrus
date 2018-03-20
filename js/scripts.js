76061145f8b2e65cf5a758b74adc2b856a6de2f1
Aaron Akbari-Mort and Nick Kardish <nick@>
2018-03-20 09:26:54 -0700

    add color changing buttons

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,18 @@ $(document).ready(function() {
     $("#initially-hidden-2").fadeToggle();
     $("#initially-showing-2").fadeToggle();
   });
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
 
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
 });
